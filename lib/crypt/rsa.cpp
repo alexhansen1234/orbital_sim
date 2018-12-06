a95c12bbb713fbecd1f45e103f9719680ce3f2ba
Alex <ajhansen@mail.usf.edu>
2018-12-06 08:45:56 -0500

    update

--- lib/crypt/rsa.cpp
+++ lib/crypt/rsa.cpp
@@ -17,46 +17,30 @@ void decrypt(char * message, uint64_t * digest, uint64_t priv_key, uint64_t mod)
 uint64_t mod_exp(uint64_t, uint64_t, uint64_t);
 
 /*
-  This should take input of a file, and read each character in to an array of type uint32_t.
-  At the start of the array, the first entry should be the length of the message to assist with
-  formatting the output from the decryption step.
-
-  example:
-
-  ./rsa -e filename1 -o my_encrypted_file -k my_key.pub
-    * encrypts filename1 using the public key my_key.pub and saves the output in a file
-    * named my_encrypted_file.
-
-  ./rsa -e filename2 -n my_key2
-    * encrypts filename2 using a newly generated public key and saves the key as filename2.pub
-    * and saves the output to filename2_encrypted.
-    * the public and private key will be saved as my_key2.pub and my_key2.priv respectively.
-
-  ./rsa -d my_encrypted_file -o filename1 -k my_key.priv
-    * decryptes my_encrypted_file using my_key.priv and saves output as filename1.
-
-  As a command-line argument, the file will be encrypted and written to a file with
-  a name specified by a -o flag. The name of the input file will be used otherwise.
-
-  One may designate a specific encryption key to use by -k flag, or a new key-pair
-  will be generated if not specified.
+  Stream cyphers are easy to figure out by character frequency analysis.
+  This is just a demonstration of the underlying algorithm used to generate
+  RSA asymmetric key pairs.
+  Block cyphers are less prone to crack by frequency analysis,
+  and professional crypto libraries are even less prone to crack :)
+  Don't roll your own crypto for deployment, kids.
 */
 
 int main(int argc, char ** argv)
 {
   srand(time(0));
   uint64_t pub, priv, n;
-  char * message = "Hello World, this is Alex and all your base are belong to us.\n";
+  char message[] = "Message to encrypt.\n";
   uint64_t digest[strlen(message)];
   generate_keys(&pub, &priv, &n);
-  printf("PubKey = %llu\nPrivKey = %llu\nN = %llu\n", pub, priv, n);
+  printf("PubKey = %llu\nPrivKey = %llu\nN = %llu\n\n", pub, priv, n);
+  printf("Original Message: %s\n", message);
   encrypt(message, digest, pub, n);
+  printf("Encrypted Message: ");
   for(int i=0; i < strlen(message); i++)
-    printf("%llu ", digest[i]);
-  printf("\n");
+    printf("0x%08llx ", digest[i]);
+  printf("\n\n");
   decrypt(message, digest, priv, n);
-  printf("\n%s\n", message);
-  printf("\n");
+  printf("Decrypted Message: %s\n", message);
   return 0;
 }
 
@@ -65,7 +49,6 @@ void decrypt(char * message, uint64_t * digest, uint64_t priv, uint64_t n)
   for(int i=0; i < strlen(message); i++)
   {
     digest[i] = mod_exp(digest[i],priv,n);
-    printf("%c ", ((uint8_t *)digest)[i]);
   }
 }
 
@@ -95,8 +78,8 @@ void generate_keys(uint64_t * pub, uint64_t * priv, uint64_t * n)
   uint64_t p, q, d;
   while(1)
   {
-    p = generate_random_prime(10000);
-    q = generate_random_prime(10000);
+    p = generate_random_prime(100000);
+    q = generate_random_prime(100000);
     /* d needs to be coprime to the totient of n, but can essentially just be another prime
        in that case, replace generate_coprime(n) with generate_random_prime(upper_bound) */
     d = generate_random_coprime((p-1)*(q-1));
@@ -145,8 +128,6 @@ uint64_t gcd(uint64_t a, uint64_t b)
   }
 }
 
-
-
 uint64_t generate_random_prime(uint64_t upper_bound)
 {
   uint64_t ret, flag=1;
@@ -160,9 +141,9 @@ uint64_t generate_random_prime(uint64_t upper_bound)
       flag = 1;
       continue;
     }
-    for(int i=2; i*i <= ret; i++)
+    for(uint64_t i=2; i*i <= ret && i != 0; i++)
     {
-      if( ! (ret % i))
+      if( ! (ret % i) )
       {
         flag = 1;
         break;
@@ -175,10 +156,10 @@ uint64_t generate_random_prime(uint64_t upper_bound)
 /* p, q are primes. d is a number that is coprime to (p-1)(q-1) */
 uint64_t generate_pub_key(uint64_t p, uint64_t q, uint64_t d)
 {
-  int32_t x_n0 = (p - 1) * (q - 1);
-  int32_t x_n1 = d;
-  int32_t quotient, rem, temp1, temp2;
-  int32_t a0 = 1, a1 = 0, b0 = 0, b1 = 1;
+  int64_t x_n0 = (p - 1) * (q - 1);
+  int64_t x_n1 = d;
+  int64_t quotient, rem, temp1, temp2;
+  int64_t a0 = 1, a1 = 0, b0 = 0, b1 = 1;
 
   while(x_n1 != 1)
   {
